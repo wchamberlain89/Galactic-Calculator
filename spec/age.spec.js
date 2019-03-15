984d854d378fd610cbafc226f060a7623b9bca18
wchamberlain89 <wchamberlain89@gmail.com>
2019-03-15 09:40:56 -0700

    Setup tests for methods determining age on different planets

---
+++ spec/age.spec.js
@@ -0,0 +1,21 @@
+import { age } from "./../src/age.js";
+
+var age = new Age(29);
+
+desribe("Years :", function(){
+	// expect("Calculate Earth Age Based on Birthdate", function(){
+	// 	it(age.earth()).toEqual(291);
+	// });
+	// expect("Calculate Venus Age Based on Birthdate", function(){
+	// 	it(age.earth()).toEqual(461);
+	// });
+	// expect("Calculate Mars Age Based on Birthdate", function(){
+	// 	it(age.earth()).toEqual(151);
+	// });
+	// expect("Calculate Jupiter Age Based on Birthdate", function(){
+	// 	it(age.earth()).toEqual(21);
+	// }); 
+	// expect("Calculate Mecury Age Based on Birthdate", function(){
+	// 	it(age.earth()).toEqual(1201);
+	// }); 
+});
--- spec/project-spec.js
+++
@@ -1,8 +0,0 @@
-import { addToTen } from './../src/js/project.js';
-
-describe('addToTen', function() {
-  it('should take two numbers that add to 10', function() {
-    let sum = addToTen(3,7);
-    expect (sum).toEqual(true);
-  });
-});
